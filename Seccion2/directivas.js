1d4c8f14c9c00f2132d9bfd1ef3273b7f3645fcb
EduardoPuli <eduardo.pulido@cceo.com.mx>
2021-12-28 16:53:37 -0600

    Activities sections

---
+++ Seccion2/directivas.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#vm',
+    data:{
+        imagen: 'http://loremflickr.com/300/200/',
+        texto: "<em>Bienvenido al curso de Vue.js 2</em>",
+        mostrar: true,
+        parrafo: 4,
+        paises: ['Mexico','Colombia','Ecuador']
+    },
+
+})
